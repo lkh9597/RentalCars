76ac218258e85abc5b9afe48ba4fe15eb06441b1
Lucas Hawkins <lkh9597@mavs.uta.edu>
2026-04-28 19:46:32 -0500

    Update Truck.java
    Added toStringAll(), modified toString, removed miles

--- Truck.java
+++ Truck.java
@@ -13,9 +13,9 @@ public class Truck extends Vehicles {
 
     public Truck(String name, Manufacturer manufacturer, int year, Color color,
                  double price, int numberOfSeats, Status status,
-                 int gasTankSize, int miles, double loadCapacity, double cargoCapacity) {
+                 int gasTankSize, double loadCapacity, double cargoCapacity) {
 
-        super(name, manufacturer, year, color, price, numberOfSeats, status, gasTankSize, miles);
+        super(name, manufacturer, year, color, price, numberOfSeats, status, gasTankSize);
         this.setLoadCapacity(loadCapacity);
         this.setCargoCapacity(cargoCapacity);
     }
@@ -44,10 +44,34 @@ public class Truck extends Vehicles {
         }
     }
 
+    @Override
     public String toString() {
-        return "TRUCK\n" +
-               super.toString() +
-               "\nLoad Capacity: " + this.loadCapacity +
-               "\nCargo Capacity: " + this.cargoCapacity;
+    	String availabilityText;
+		
+		if(this.isAvailable()) {
+			availabilityText =  "Free";
+		}else {
+			availabilityText = "Rented";
+		}
+
+	    return String.format(
+	        "%-7s: %-10s | %-8s | Price: $%8.2f | Seats: %2d | Cargo: %6.1f | Load: %6.1f | Status: %-10s",
+	        "Truck",
+	        this.getLicensePlate(),
+	        availabilityText,
+	        this.getPrice(),
+	        this.getNumberOfSeats(),
+	        this.getCargoCapacity(),
+	        this.getLoadCapacity(),
+	        this.getStatus()
+	    );
+    }
+    
+    @Override
+    public String toStringAll() {
+    	
+    	return super.toStringAll() +
+    		   "\nCargo Capacity: " + this.getCargoCapacity() +
+    	       "\nLoad Capacity: " + this.getLoadCapacity();
     }
 }
